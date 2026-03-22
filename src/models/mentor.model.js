fffedd7d0898535e18fd4f47b6fc953aba11e493
Shrawan <shrwnkumar08@gmail.com>
2026-03-22 22:10:54 +0530

    mentor schema added

---
+++ src/models/mentor.model.js
@@ -0,0 +1,68 @@
+import mongoose from "mongoose";
+
+const AvailabilitySlotSchema = new mongoose.Schema(
+  {
+    day: {
+      type: String,
+      enum: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
+      required: true,
+    },
+  },
+  { _id: true },
+);
+
+const MentorSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+    unique: true,
+  },
+  headline: {
+    type: String,
+    maxlength: 120,
+  },
+  expertise: [
+    {
+      type: string,
+      trim: true,
+    },
+  ],
+  experience: {
+    type: Number,
+    default: 0,
+  },
+  language: [
+    {
+      type: String,
+    },
+  ],
+  availability: [AvailabilitySlotSchema],
+
+  //stats (updated by aggregation or manually)
+
+  totalSessions: {
+    type: Number,
+    default: 0,
+  },
+  avgRating: {
+    type: Number,
+    default: 0,
+    min: 0,
+    max: 5,
+  },
+  totalReviews: { type: Number, default: 0 },
+  isApproved: {
+    type: Boolean,
+    default: false,
+  },
+  isActive: {
+    type: Boolean,
+    default: true,
+  },
+},{timestamps : true});
+
+
+const Mentor = mongoose.model("Mentor", MentorSchema);
+
+export default Mentor;
