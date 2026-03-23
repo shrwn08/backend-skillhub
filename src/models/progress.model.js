8687a7784faca16c30fa0beea17b921e92f2247a
Shrawan <shrwnkumar08@gmail.com>
2026-03-23 19:25:54 +0530

    progressbar added

---
+++ src/models/progress.model.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const stepProgressSchema = new mongoose.Schema({
+    stepId : {type : mongoose.Schema.Types.ObjectId, required : true},
+    stepNumber : {type : Number},
+    completed : {type : Boolean, default : false},
+    completedAt : {type : date},
+}, {_id : false});
+
+
+const ProgressShcema = new mongoose.Schema({
+    user : {type : mongoose.Schema.Types.ObjectId, ref : "User", required : true},
+    idea : {
+        type : mongoose.Schema.Types.ObjectId, ref : "Idea", required : true
+    },
+    steps : [stepProgressSchema],
+    isCompleted : {type : Boolean, default: false},
+    completedAt : {type : Data},
+},{timestamps : true});
+
+
+ProgressShcema.index({user: 1, idea : 1}, {unique : true});
+
+const Progress = mongoose.model("Progress", ProgressShcema);
+
+export default Progress;
